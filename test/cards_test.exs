02a5080c53511f63fa9af801d6d18ad11d21e759
Josh Parker <joshBBparker@gmail.com>
2021-07-21 23:42:31 -0600

    add first test for create deck

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,8 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "can create deck with one card" do
+    assert Cards.create_deck(["one"], ["only"]) == [%{rank: "one", suit: "only"}]
   end
+
 end
